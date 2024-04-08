7ce44705a4453cdf3fddc2e68a6b314f4c3e50e4
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2024-04-08 20:18:06 -0300

    criando projeto base

---
+++ src/main/java/com/contact/finder/ContactFinder/ContactFinderApplication.java
@@ -0,0 +1,13 @@
+package com.contact.finder.ContactFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contact/finder/ContactFinder/ContactFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contact.finder.ContactFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
