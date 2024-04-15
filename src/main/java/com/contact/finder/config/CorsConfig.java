49a883499e27d191960b6beb43b5cddc927d3680
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2024-04-15 13:41:24 -0300

    reajustando cors

--- src/main/java/com/contact/finder/config/CorsConfig.java
+++ src/main/java/com/contact/finder/config/CorsConfig.java
@@ -1,20 +1,17 @@
 package com.contact.finder.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-	 @Bean
-	    public WebMvcConfigurer corsConfigurer() {
-	        return new WebMvcConfigurer() {
-	            @Override
-	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("/**") 
-	                        .allowedOrigins("https://contact-finder-production.up.railway.app");
-	            }
-	        };
-	    }
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://seu-site.com") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE") 
+                .allowedHeaders("*"); //
+    }
 }
